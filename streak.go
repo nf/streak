7ab92a9ca93f3c25b127a208555bd18104dd058a
Andrew Gerrand <adg@golang.org>
2013-01-06 15:53:47 +1100

    Add docs

--- streak.go
+++ streak.go
@@ -1,9 +1,3 @@
-// Copyright 2011 The goauth2 Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// This program makes a call to the specified API, authenticated with OAuth2.
-// a list of example APIs can be found at https://code.google.com/oauthplayground/
 package main
 
 import (
